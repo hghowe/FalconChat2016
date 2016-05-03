dab07ec2a729f544e8734aa4686ab8894e1fbe33
Harlan Howe <hghowe@gmail.com>
2016-05-03 18:57:33 -0500

    Constructor with layout - a JPanel inside a ScrollPanel, centered; plus a JTextField in the south.

--- FalconChat2016/src/FalconChatFrame.java
+++ FalconChat2016/src/FalconChatFrame.java
@@ -1,16 +1,35 @@
+import java.awt.BorderLayout;
 import java.awt.GraphicsConfiguration;
 import java.awt.HeadlessException;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 public class FalconChatFrame extends JFrame {
 
+	private JPanel myPanel;
+	private JTextField myTextField;
+	
 	public FalconChatFrame() {
 		super("Falcon Chat!");
 		setSize(400,600);
 		setResizable(true);
+		setupLayout();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+	}
+	
+	private void setupLayout()
+	{
+		getContentPane().setLayout(new BorderLayout());
+		myPanel = new JPanel();
+		myTextField = new JTextField();
+		JScrollPane sp = new JScrollPane();
+		sp.add(myPanel);
+		getContentPane().add(sp, BorderLayout.CENTER);
+		getContentPane().add(myTextField, BorderLayout.SOUTH);
 	}
 
 

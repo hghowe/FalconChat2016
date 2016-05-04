e3fa57155c1ec4edf13c4eabe56bd836fa7f9add
Harlan Howe <hghowe@gmail.com>
2016-05-04 09:32:17 -0500

    Removed Silliness from Runner.

--- FalconChat2016/src/FalconChatRunner.java
+++ FalconChat2016/src/FalconChatRunner.java
@@ -7,8 +7,6 @@ public class FalconChatRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, world.");
-		System.out.println("Save the Welsh!");
 		FalconChatFrame theApp = new FalconChatFrame();
 	}
 

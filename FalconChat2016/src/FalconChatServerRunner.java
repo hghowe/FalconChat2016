296740076104adf3dbe1bef2953623d980f6a408
Harlan Howe <hghowe@gmail.com>
2016-05-03 21:08:07 -0500

    Removed "run" command - this will be handled in the server's constructor.

--- FalconChat2016/src/FalconChatServerRunner.java
+++ FalconChat2016/src/FalconChatServerRunner.java
@@ -8,7 +8,6 @@ public class FalconChatServerRunner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		FalconChatServer theApp = new FalconChatServer();
-		theApp.run();
 	}
 
 }

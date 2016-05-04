2b2d80bcdd1afd9fb97a656b2c02f1af6085ab0d
Harlan Howe <hghowe@gmail.com>
2016-05-03 23:29:37 -0500

    Added a displayMessage() method to update the textArea (presumably with messages from the server).

--- FalconChat2016/src/FalconChatFrame.java
+++ FalconChat2016/src/FalconChatFrame.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -15,16 +16,20 @@ public class FalconChatFrame extends JFrame implements ActionListener {
 
 	private JTextArea myPanel;
 	private JTextField myTextField;
+	private FalconChatClient myConnection;
 	
 	public FalconChatFrame() {
 		super("Falcon Chat!");
 		setSize(400,600);
 		setResizable(true);
 		setupLayout();
+		myConnection = new FalconChatClient(requestName(), this);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
-	
+	/**
+	 * sets up the GUI
+	 */
 	private void setupLayout()
 	{
 		getContentPane().setLayout(new BorderLayout());
@@ -36,14 +41,32 @@ public class FalconChatFrame extends JFrame implements ActionListener {
 		getContentPane().add(myTextField, BorderLayout.SOUTH);
 		myTextField.addActionListener(this);
 	}
-
+	
+	public void displayMessage(String message)
+	{
+		myPanel.setText(myPanel.getText()+"\n"+message);
+		
+	}
+	
+	/**
+	 * asks the user for his/her name. 
+	 * @return a non-empty String.
+	 */
+	public String requestName()
+	{
+		String name;
+		do
+		{
+		   name = JOptionPane.showInputDialog("Please enter your name.");
+		} while (name.equals(""));
+		return name;
+	}
 	
 	public void actionPerformed(ActionEvent ae)
 	{
 		if (ae.getSource() == myTextField)
 		{
 			System.out.println(myTextField.getText());
-//			myPanel.setText(myPanel.getText()+"\n"+myTextField.getText());
 			myTextField.setText("");
 			
 		}

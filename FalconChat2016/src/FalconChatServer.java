feefd09df8afa1be8892ec8c876d4da1349f0e84
Harlan Howe <hghowe@gmail.com>
2016-05-03 21:11:12 -0500

    Set up constructor.

--- FalconChat2016/src/FalconChatServer.java
+++ FalconChat2016/src/FalconChatServer.java
@@ -1,12 +1,26 @@
+import java.net.ServerSocket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
 import java.util.TimerTask;
 
 public class FalconChatServer extends TimerTask
 {
-
+	private int nextAvailableID;
+	private ServerSocket mySocket;
+	private Map<Integer, FalconChatClient> clients;
+	
 	public FalconChatServer() {
-		// TODO Auto-generated constructor stub
-		
+		super();
+		nextAvailableID = 0;
+		Timer t = new Timer();
+		t.scheduleAtFixedRate(this, 0, 20);  // this is the TimerTask class whose run()
+											// method will be called. 0 is the delay before
+											// the method is called first; 20 is the delay
+											// (in ms) between calls of run().
+		clients = new HashMap<Integer, FalconChatClient>();
 	}
+
 	
 	public void run()
 	{

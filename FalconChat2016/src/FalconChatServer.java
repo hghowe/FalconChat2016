5bb479727900db222e525965a119f390a5bf42e3
Harlan Howe <hghowe@gmail.com>
2016-05-03 21:01:31 -0500

    Created FalconChatServer

---
+++ FalconChat2016/src/FalconChatServer.java
@@ -0,0 +1,17 @@
+import java.util.TimerTask;
+
+public class FalconChatServer extends TimerTask
+{
+
+	public FalconChatServer() {
+		// TODO Auto-generated constructor stub
+		
+	}
+	
+	public void run()
+	{
+		
+		
+	}
+
+}

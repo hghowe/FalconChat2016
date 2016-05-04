a16748d5b294d16e3750b0cdf2dd3df772230e09
Harlan Howe <hghowe@gmail.com>
2016-05-03 22:19:22 -0500

    Created Chatterer - this class represents the clients in this program - each has a name, ID, and a printwriter. The last of these is so that we can send messages to the client computer this Chatterer represents.

---
+++ FalconChat2016/src/Chatterer.java
@@ -0,0 +1,32 @@
+import java.io.PrintWriter;
+
+/**
+ * This is a class that is used by the FalconChatServer to represent one of the people talking
+ * in this chat.
+ * @author harlan.howe
+ *
+ */
+public class Chatterer {
+
+	private String myName;
+	private int myID;
+	private PrintWriter myPrintWriter;
+	
+	public Chatterer(String name, int id, PrintWriter pw) 
+	{
+		myName = name;
+		myID = id;
+		myPrintWriter = pw;
+	}
+	
+	public String getName() {return myName;}
+	public int getID() {return myID;}
+	
+	public void sendMessage(String message)
+	{
+		myPrintWriter.println(message);
+		myPrintWriter.flush();
+	}
+	
+
+}

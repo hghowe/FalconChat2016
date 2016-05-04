9a30cb3c9b51e4bace30a29989efea4704759694
Harlan Howe <hghowe@gmail.com>
2016-05-03 21:01:14 -0500

    Created FalconChatServerRunner

---
+++ FalconChat2016/src/FalconChatServerRunner.java
@@ -0,0 +1,14 @@
+
+public class FalconChatServerRunner {
+
+	public FalconChatServerRunner() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FalconChatServer theApp = new FalconChatServer();
+		theApp.run();
+	}
+
+}
